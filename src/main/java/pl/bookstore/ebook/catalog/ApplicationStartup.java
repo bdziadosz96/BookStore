ab1d89f8e1930949767620f6441fabda995dd421
bdziadosz96 <bdziadosz96@icloud.com>
2021-11-06 15:35:09 +0100

    Create new architecture

--- src/main/java/pl/bookstore/ebook/catalog/ApplicationStartup.java
+++ src/main/java/pl/bookstore/ebook/catalog/ApplicationStartup.java
@@ -3,6 +3,7 @@ package pl.bookstore.ebook;
 import java.util.List;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 import pl.bookstore.ebook.catalog.application.controller.CatalogController;
@@ -14,9 +15,18 @@ class ApplicationStartup implements CommandLineRunner {
 
   private final CatalogController controller;
 
+  @Value("${pl.bookstore.query:default}")
+  private String title;
+
+  @Value("${pl.bookstore.limit:3}")
+  private Long limit;
+
   @Override
   public void run(final String... args) throws Exception {
-    List<Book> books = controller.findByTitle("Pan");
+    List<Book> books = controller.findByTitle(title)
+            .stream()
+            .limit(limit)
+            .toList();
     books.forEach(System.out::println);
   }
 }
