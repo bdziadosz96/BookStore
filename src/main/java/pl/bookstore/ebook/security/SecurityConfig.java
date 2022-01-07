5db6904c4323e5854c317f87837f244b30964991
bdziadosz96 <bdziadosz96@icloud.com>
2022-01-07 23:15:15 +0100

    Basic auth

---
+++ src/main/java/pl/bookstore/ebook/security/SecurityConfig.java
@@ -0,0 +1,26 @@
+package pl.bookstore.ebook.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .mvcMatchers(HttpMethod.GET,"/catalog/**")
+                .permitAll()
+                .mvcMatchers(HttpMethod.POST,"/admin/initialize")
+                .permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .httpBasic()
+                .and()
+                .csrf().disable();
+
+    }
+}
