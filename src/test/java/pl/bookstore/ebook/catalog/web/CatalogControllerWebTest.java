c3a6b83e03b6b93b199d0e61cc430e675874fb89
bdziadosz96 <bdziadosz96@icloud.com>
2022-01-18 20:09:02 +0100

    Travis CI

--- src/test/java/pl/bookstore/ebook/catalog/web/CatalogControllerWebTest.java
+++ src/test/java/pl/bookstore/ebook/catalog/web/CatalogControllerWebTest.java
@@ -7,7 +7,6 @@ import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import pl.bookstore.ebook.catalog.app.port.CatalogUseCase;
